c9c3ce7c81302708ffb9209d74cf0cef9e81862c
netolobo <desilio@gmail.com>
2023-11-26 09:03:25 -0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Neto Lobo on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
